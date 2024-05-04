88c1d4f504459a270cc7065a24959c575f634af7
Jhuli Spindola <jhuspindola@gmail.com>
2024-05-04 12:46:03 -0300

    Novas alterações no projeto!

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-import http from "http";
+import "dotenv/config";
+import app from "./src/app.js";
 
 const PORT = 5500;
 
-const rotas = {
-    "/": "Curso de Express API",
-    "/livros": "Entrei na rota dos livros",
-    "/autores": "Entrei na rota dos autores"
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Servidor escutando!");
 });
